5cb0b7a331de0d3f2fec54b20b9ff641b195f8e7
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-05-24 15:35:14 +0700

    feat(db): :sparkles: interface to interact with database
    specifically, to interact with `ArticleCategories` table

---
+++ src/services/postgresql/ArticleCategoriesService.ts
@@ -0,0 +1,30 @@
+import {PrismaClient} from '@prisma/client';
+import {prisma} from './client';
+import InvariantError from '../../exceptions/InvariantError';
+import {ArticleCategoryPayload} from '../../types/article-category-payload';
+import {nanoid} from 'nanoid';
+
+export class ArticleCategoriesService {
+  private prisma: PrismaClient;
+  constructor() {
+    this.prisma = prisma;
+  }
+
+  addArticleCategory = async ({name}: ArticleCategoryPayload) => {
+    const id = `article-category-${nanoid(8)}`;
+    const lowername = name.toLowerCase();
+
+    const newCategory = await prisma.articleCategories.create({
+      data: {
+        id,
+        name: lowername,
+      },
+    });
+
+    if (!newCategory.id) {
+      throw new InvariantError('Failed to add new article category');
+    }
+
+    return newCategory;
+  };
+}

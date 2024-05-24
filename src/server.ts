61b0787d78d55cca8de54d06d1eea58637c2579f
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-05-24 15:29:15 +0700

    feat(endpoint): :sparkles: register articlecategories plugin

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
-import {Server, ServerOptions, ServerRegisterPluginObject} from '@hapi/hapi';
+import {
+  Server,
+  ServerOptions,
+  ServerRegisterPluginObject,
+  Request,
+  ResponseToolkit,
+  ResponseObject,
+} from '@hapi/hapi';
 import * as HapiSwagger from 'hapi-swagger';
 import * as Inert from '@hapi/inert';
 import * as Vision from '@hapi/vision';
 import helloworld from './apis/helloworld';
+import articlecategories from './apis/articlecategories';
+import ClientError from './exceptions/ClientError';
 
 const options: ServerOptions = {
   port: process.env.PORT || 3821,
@@ -26,15 +35,39 @@ export const init = async () => {
     Inert,
     Vision,
     {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    },
+    {
       plugin: helloworld,
     },
     {
-      plugin: HapiSwagger,
-      options: swaggerOptions,
+      plugin: articlecategories,
     },
   ];
 
   await server.register(plugins);
+
+  server.ext(
+    'onPreResponse',
+    (request: Request, h: ResponseToolkit): ResponseObject | symbol => {
+      const response = request.response;
+
+      if (response instanceof ClientError) {
+        return h
+          .response({
+            status: 'fail',
+            message: response.message,
+          })
+          .code(response.statusCode);
+      }
+
+      console.log(request);
+      console.log(response);
+
+      return h.continue;
+    }
+  );
 
   server.start();
   console.log(`Server is running on ${server.info.uri}`);

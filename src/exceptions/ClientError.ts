9ce669994b4bf7e31096518b58c1399381749545
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-05-25 02:27:13 +0700

    style: :lipstick: improve readability

--- src/exceptions/ClientError.ts
+++ src/exceptions/ClientError.ts
@@ -1,6 +1,7 @@
 export default class ClientError extends Error {
   public statusCode: number;
   public name: string;
+
   constructor(message: string, statusCode = 400) {
     super(message);
     this.statusCode = statusCode;

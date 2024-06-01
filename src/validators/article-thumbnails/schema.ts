6f1a608cff0814aaa1c88f95004eace14a95709c
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-06-01 21:01:30 +0700

    refactor: :recycle: update `multipart/form-data` schema

--- src/validators/article-thumbnails/schema.ts
+++ src/validators/article-thumbnails/schema.ts
@@ -1,14 +1,19 @@
 import * as Joi from 'joi';
 
 export const postImageHeaderSchema = Joi.object({
-  'content-type': Joi.string()
-    .valid(
-      'image/apng',
-      'image/avif',
-      'image/gif',
-      'image/jpeg',
-      'image/png',
-      'image/webp'
-    )
-    .required(),
-}).unknown();
+  hapi: Joi.object({
+    filename: Joi.string().required(),
+    headers: Joi.object({
+      'content-type': Joi.string()
+        .valid(
+          'image/apng',
+          'image/avif',
+          'image/gif',
+          'image/jpeg',
+          'image/png',
+          'image/webp'
+        )
+        .required(),
+    }).unknown(true),
+  }).unknown(true),
+}).unknown(true);
